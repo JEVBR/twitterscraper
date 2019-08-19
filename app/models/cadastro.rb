b64821e3aaea1b9b8d9eb379aef184bdd0a5f4b2
Jeroen Haisma <J_Haisma@hotmail.com>
2019-08-18 21:29:54 -0300

    added short url and more scss

--- app/models/cadastro.rb
+++ app/models/cadastro.rb
@@ -2,23 +2,18 @@ class Cadastro < ApplicationRecord
   validates :name, presence: true
   validates :url, presence: true
 
-  after_validation :default, :scraper, :schrink_url
+  after_validation :scraper, :schrink_url
 
   private
 
-  def default
-    # after validation went well, get the data from Twitter and make a short URL
+  def scraper
     self.scraped_title = 'title not found'
     self.scraped_description = 'description not found'
     self.scraped_user = '@user not found'
-  end
 
-  def scraper
-    # Url not on twitter: (could make this as a validation)
     return unless url.match?(/\A(https:\/\/twitter.com\/)/)
 
     page = HTTParty.get(url)
-    # keep the "not found" msg if party not good (200.299 range = good)
     return unless (200..299).cover?(page.code)
 
     doc = Nokogiri::HTML(page)
@@ -28,6 +23,10 @@ class Cadastro < ApplicationRecord
   end
 
   def schrink_url
-    self.short_url = 'VeryshortURL'
+    return if short_url.present?
+
+    party_url = "https://is.gd/create.php?format=simple&url=#{url}"
+    r = HTTParty.post(party_url)
+    self.short_url = (200..299).cover?(r.code) ? r.parsed_response : 'No short URL available'
   end
 end
